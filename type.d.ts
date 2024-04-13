7cdc9d0cac12712e8ad23326dd7f0c14225d9ffd
Lamine Diallo <bah07565@gmail.com>
2024-04-13 05:18:38 +0000

    add a type.d.ts

---
+++ type.d.ts
@@ -0,0 +1,21 @@
+type IpDetailsType = {
+  ip: string;
+  location: {
+    country: string;
+    region: string;
+    city: string;
+    lat: number;
+    lng: number;
+    postalCode: string;
+    timezone: string;
+    geonameId: number;
+  };
+  as: {
+    asn: number;
+    name: string;
+    route: string;
+    domain: string;
+    type: string;
+  };
+  isp: string;
+}
