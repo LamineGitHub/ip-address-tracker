0c07e92c47cfd09f98d875953c03fd9cc45b1fb5
Lamine Diallo <bah07565@gmail.com>
2024-04-15 08:16:18 +0000

    Feat: Remove fake data for a real API call

--- src/ts/ipify.ts
+++ src/ts/ipify.ts
@@ -16,64 +16,16 @@ export const fetchIpDetails = async (
   domain = "",
 ): Promise<IpDetailsType | null> => {
   const url = `${baseUrl}&ipAddress=${ipAddress}&domain=${domain}`;
-  console.log(url);
 
   try {
-    // const response = await fetch(url)
-    // if (!response.ok) {
-    //   console.error(`Erreur HTTP! Statut: ${ response.status }`)
-    //   return
-    // }
-    let ipDetails: IpDetailsType;
+    const response = await fetch(url);
 
-    if (ipAddress) {
-      ipDetails = {
-        ip: "185.177.125.65",
-        location: {
-          country: "NL",
-          region: "Provincie Noord-Holland",
-          city: "Amsterdam-Duivendrecht",
-          lat: 52.31926,
-          lng: 4.92569,
-          postalCode: "1114",
-          timezone: "+02:00",
-          geonameId: 11746796,
-        },
-        as: {
-          asn: 49981,
-          name: "WorldStream",
-          route: "185.177.125.0/24",
-          domain: "https://www.worldstream.com",
-          type: "Network Services",
-        },
-        isp: "WorldStream B.V.",
-      };
-    } else {
-      ipDetails = {
-        ip: "102.164.183.160",
-        location: {
-          country: "SN",
-          region: "Dakar",
-          city: "Niaye Tioker",
-          lat: 14.6725,
-          lng: -17.44361,
-          postalCode: "",
-          timezone: "+00:00",
-          geonameId: 2595755,
-        },
-        as: {
-          asn: 37649,
-          name: "Tigo",
-          route: "102.164.183.0/24",
-          domain: "free.sn",
-          type: "",
-        },
-        isp: "Tigo Public IP",
-      };
+    if (!response.ok) {
+      console.error(`HTTP error! Status: ${response.status}`);
+      return null;
     }
-    // }
-    return ipDetails;
-    // return await response.json()
+
+    return await response.json();
   } catch (error) {
     console.error("Error during data recovery :", error);
     return null;
